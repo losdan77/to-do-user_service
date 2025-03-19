a1173bd925e311f482128ab480762e5849ae7381
losdan <losdan322@gmail.com>
2025-03-19 22:41:18 +0300

    add code(15m) in redis

--- app/users/router.py
+++ app/users/router.py
@@ -1,10 +1,12 @@
+from random import randint
 from fastapi import APIRouter, Response, Depends
+from app.cache import redis
 from app.auth.auth import authenticate_user, get_password_hash
 from app.auth.dependecies import get_current_user
 from app.exceptions import VerifyOldPasswordException
 from app.users.dao import UsersDAO
 from app.users.models import Users
-from app.users.schemas import SChangePassword
+from app.users.schemas import SChangePassword, SForgotPassword, SVerifyCode
 
 router = APIRouter(prefix='/users',
                    tags=['Пользователи'],)
@@ -35,3 +37,19 @@ async def get_profile_by_id(profile_id: str):
         pass
 
     return profile
+
+
+@router.post('/forgot_password')
+async def forgot_password(user_data: SForgotPassword):
+    code = randint(100000, 999999)
+    redis.set(user_data.email, code, 60*15)
+    return 'ok'
+
+
+@router.post('/verify_code')
+async def verify_code(user_data: SVerifyCode):
+    saved_code = redis.get(user_data.email)
+    if user_data.code == int(saved_code):
+        redis.delete(user_data.email)
+        return True
+    return False
